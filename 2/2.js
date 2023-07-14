62b1b7f7c63ecdbcf6b6bc1184783525bd4f8d89
Veronika0123 <veronikamokrii@gmail.com>
2023-07-14 22:50:26 +0300

    2

--- 2/2.js
+++ 2/2.js
@@ -1,8 +1,8 @@
-    let length = +prompt('Сторона ромба в см.');
-    console.log(`Сторона ромба ${length} см.`);
+    let length = +prompt('Ребро куба в см.');
+    console.log(`Ребро куба ${length} см.`);
 
     let n = (length*length)*6;
-    console.log(`Площадь ромба ${n} см^2.`);
+    console.log(`Площадь куба ${n} см^2.`);
 
     let volume = (length**3);
-    console.log(`Объем ромба ${volume} см^3.`);
+    console.log(`Объем куба ${volume} см^3.`);
